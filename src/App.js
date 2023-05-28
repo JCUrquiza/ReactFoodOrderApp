c97141594b7651fccb81ddb1636821125d37d939
Juan <gurquiza8@gmail.com>
2023-05-28 15:24:08 -0500

    modal closed and oppened with events onClose

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
+
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  }
+
   return (
     <>
-      <Header />
+      {
+        cartIsShow && <Cart onClose={hideCartHandler} />
+      }
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,10 +1,10 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
 
-const HeaderCardButton = () => {
+const HeaderCardButton = (props) => {
 
     return (
-        <button className={ classes.button }>
+        <button className={ classes.button } onClick={ props.onClick }>
             <span className={ classes.icon }>
                 <CartIcon />
             </span>
