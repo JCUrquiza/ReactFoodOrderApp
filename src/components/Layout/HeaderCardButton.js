cf65bdc23a24dda5e670ac623c56d1d1366670c4
Juan <gurquiza8@gmail.com>
2023-05-28 15:58:30 -0500

    adding usereducer

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCardButton = (props) => {
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
 
     return (
         <button className={ classes.button } onClick={ props.onClick }>
@@ -12,7 +20,7 @@ const HeaderCardButton = (props) => {
                 Your Cart
             </span>
             <span className={ classes.badge }>
-                3
+                { numberOfCartItems }
             </span>
         </button>
     )
