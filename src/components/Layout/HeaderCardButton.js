7c7c3ba2aec14ca56a32c81cc63672db7f63c0ad
Juan <gurquiza8@gmail.com>
2023-05-28 22:57:12 -0500

    adding effect on the button by add meal to the app

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,18 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCardButton = (props) => {
 
-    const cartCtx = useContext(CartContext);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const cartCtx = useContext(CartContext);
+    
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    // useHook para animación de button
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        // El estado que condiciona el button lo devolvemos a false.
+        // Con un settimer de 300 milisegundos porque es lo que dura la animación:
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+        // Se considera una buena práctica limpiar el temporizador:
+        return () => clearTimeout(timer);
+        
+    }, [items]);
+    
     return (
-        <button className={ classes.button } onClick={ props.onClick }>
+        <button className={btnClasses} onClick={ props.onClick }>
             <span className={ classes.icon }>
                 <CartIcon />
             </span>
