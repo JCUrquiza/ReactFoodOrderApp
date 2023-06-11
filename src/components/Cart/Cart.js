4e74eb758276992d6e810895635a359f8dd3847f
Juan <gurquiza8@gmail.com>
2023-06-10 22:34:45 -0500

    Adding form by adress with useRef hook

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+
+    const [isCheckout, setIsCheckout] = useState(false);
 
     const cartCtx = useContext(CartContext);
 
@@ -19,6 +22,10 @@ const Cart = (props) => {
         cartCtx.addItem({...item, amount: 1});
     };
 
+    const orderHandler = () => {
+        setIsCheckout(true);
+    }
+
     const cartItems = <ul className={ classes['cart-items'] }>{
         cartCtx.items.map( (item) => 
             <CartItem 
@@ -31,6 +38,13 @@ const Cart = (props) => {
             />
         )}</ul>;
 
+    const modalActions = 
+        <div className={ classes.actions }>
+            <button className={ classes['button--alt'] } onClick={props.onClose} >Close</button>
+            { hasItems && <button className={ classes.button } onClick={orderHandler}>Order</button> }
+        </div>
+    
+
     return (
         <Modal onClose={ props.onClose }>
             { cartItems }
@@ -38,10 +52,11 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{ totalAmount }</span>
             </div>
-            <div className={ classes.actions }>
-                <button className={ classes['button--alt'] } onClick={props.onClose} >Close</button>
-                { hasItems && <button className={ classes.button }>Order</button> }
-            </div>
+
+            { isCheckout && <Checkout onCancel={props.onClose} /> }
+
+            { !isCheckout && modalActions }
+            
         </Modal>
     )
 }
